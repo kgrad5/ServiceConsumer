9e7693e0bfe33bbb9387bffd97128238c6cc9488
Kevin Grad <kgrad5@gmail.com>
2013-05-05 11:15:08 -0400

    initial commit.

---
+++ src/main/java/com/kgrad/serviceconsumer/HelloWorld.java
@@ -0,0 +1,57 @@
+package com.kgrad.serviceconsumer;
+
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorld extends AbstractHandler
+{
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException
+    {
+
+        HttpClient httpclient = new DefaultHttpClient();
+        try {
+            HttpGet httpget = new HttpGet("http://localhost:8080/list-directory");
+
+            System.out.println("executing request " + httpget.getURI());
+
+            // Create a response handler
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+            baseRequest.setHandled(true);
+            response.getWriter().println(responseBody);
+
+        } finally {
+            httpclient.getConnectionManager().shutdown();
+        }
+
+
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8888);
+        server.setHandler(new HelloWorld());
+
+        server.start();
+        server.join();
+    }
+}
